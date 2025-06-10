855828880acfd7062aa1eefe6f479e5daf1dc94a
Leo <leducl@3il.fr>
2025-06-10 15:29:42 +0200

    Create server.py

---
+++ etape_3/server.py
@@ -0,0 +1,20 @@
+import socket
+
+serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serveur.bind(("0.0.0.0", 63000))
+serveur.listen(1)
+print("Serveur en attente de connexion...")
+
+while True:
+    conn, addr = serveur.accept()
+    print("Client connecté:", addr)
+    
+    while True:
+        msg = conn.recv(1024).decode()
+        if msg.lower() == "fin":
+            break
+        print("Message reçu:", msg)
+        conn.send(msg.encode())
+    
+    print("Déconnexion de", addr)
+    conn.close()
